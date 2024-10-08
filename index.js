20db3a35f4ca23ec3baee409017a5a7b60cd92eb
ShreeramNew <shreerambca1@gmail.com>
2024-10-08 14:29:30 +0530

    updated cors

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const ConnectToMongo=require("./database");
-require('dotenv').config();
+const ConnectToMongo = require("./database");
+require("dotenv").config();
 ConnectToMongo();
+const cors = require("cors");
 
-const express=require('express');
-const app=express();
-const port=process.env.PORT
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
 
-app.use(express.json())
-app.use("/login",require("./routes/Login"));
-app.use("/signup",require("./routes/SignUp"));
+app.use(cors({ origin: "*", credentials: true }));
+app.use(express.json());
+app.use("/login", require("./routes/Login"));
+app.use("/signup", require("./routes/SignUp"));
 
-app.listen(port,()=>{
-    console.log("Ready to listen!");
-})
+app.listen(port, () => {
+   console.log("Ready to listen!");
+});
