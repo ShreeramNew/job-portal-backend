baa4e040b3cc7cad8372cd350cb28070e634fb91
ShreeramNew <shreerambca1@gmail.com>
2024-12-29 14:55:59 +0530

    added main domain to CORS allow list

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT;
 //-----------------------------------Middlewares----------------------------------------
 app.use(cookieParser());
 
-app.use(cors({ origin: ["http://localhost:3001", "http://192.168.1.5:3001"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:3001", "http://192.168.1.5:3001","https://jobnow24.vercel.app"], credentials: true }));
 app.use(express.json());
 
 app.listen(port, () => {
